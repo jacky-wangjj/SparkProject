385bbc185a1fb7b10d8a278ad085e8ea26c7d4d9
wangjj17 <wangjj17@lenovo.com>
2019-07-04 19:56:38 +0800

    spark wordcount实例

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,6 +1,8 @@
 /**
   * Created by wangjj17 on 2019/1/7.
   */
+
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Test {
@@ -8,16 +10,20 @@ object Test {
     val conf = new SparkConf().setAppName("mySpark")
     conf.setMaster("local")
     val sc = new SparkContext(conf)
-    val rdd = sc.parallelize(List(1, 2, 3, 4, 5, 6, 7, 8, 9)).map(_*3)
-    val mappedRDD = rdd.filter(_>10).collect()
+    //生成RDD有两种方式：
+    //1.sc.parallelize()
+    //2.sc.textFile()
+    val rdd: RDD[Int] = sc.parallelize(List(1, 2, 3, 4, 5, 6, 7, 8, 9)).map(_ * 3)
+    val filtered: Array[Int] = rdd.filter(_ > 10).collect()
     //对集合求和
-    println(rdd.reduce(_+_))
+    println(rdd.reduce(_ + _))
     //输出大于10的元素
-    for(arg <- mappedRDD) {
-      print(arg+" ")
+    for (arg <- filtered) {
+      print(arg + " ")
     }
     println()
   }
+
   def main(args: Array[String]) {
     println("hello world!")
     test()
